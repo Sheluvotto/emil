4f4a55ecef54d9ede2d70bd656793dd5be2c1612
Sheluvotto <sheluvemil0.1@gmail.com>
2025-03-17 00:13:56 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+      screens: {
+        'xs': {'max': '475px'},
+        'sm': {'max': '640px'},
+        'md': {'max': '768px'},
+        'lg': {'max': '1024px'},
+        'xl': {'max': '1280px'},
+      },
+    },
+  },
+  plugins: [],
+};
